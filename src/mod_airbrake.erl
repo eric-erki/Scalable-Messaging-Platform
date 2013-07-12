bae7334288784349ac3bef95b65a203114117e36
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2013-07-12 21:11:36 +1000

    Fix airbrake support

--- src/mod_airbrake.erl
+++ src/mod_airbrake.erl
@@ -25,32 +25,24 @@
 -define(ENVIRONMENT, "Development").
 
 start(_Host, Opts) ->
-    NotificationApi = gen_mod:get_opt(notification_api,
-				      Opts, ?NOTIFICATION_API),
-    ApiKey = gen_mod:get_opt(apikey, Opts, undefined),
+    NotificationApi = gen_mod:get_opt(notification_api, Opts,
+                                      fun check_fun/1, ?NOTIFICATION_API),
+    ApiKey = gen_mod:get_opt(apikey, Opts, fun check_fun/1),
     Environment = gen_mod:get_opt(environment, Opts,
-				  ?ENVIRONMENT),
+                                  fun check_fun/1, ?ENVIRONMENT),
     case ApiKey of
-      undefined ->
-	  ?ERROR_MSG("Cannot start mod_airbrake: Missing apikey", []);
-      Key when is_binary(Key) ->
-	  ensure_started(crypto),
-	  ensure_started(public_key),
-	  ensure_started(ssl),
-	  application:load(erlbrake),
-	  application:set_env(erlbrake, notification_api, NotificationApi),
-	  application:set_env(erlbrake, apikey, ApiKey),
-	  application:set_env(erlbrake, environment, Environment),
-	  application:set_env(erlbrake, error_logger, true),
-	  erlbrake:start()
+        undefined ->
+            ?ERROR_MSG("Cannot start mod_airbrake: Missing apikey", []);
+        _ ->
+            application:load(erlbrake),
+            application:set_env(erlbrake, notification_api, NotificationApi),
+            application:set_env(erlbrake, apikey, ApiKey),
+            application:set_env(erlbrake, environment, Environment),
+            application:set_env(erlbrake, error_logger, true),
+            ejabberd:start_app(erlbrake)
     end.
 
 stop(_Host) -> ok.
 
-ensure_started(App) ->
-    case application:start(App) of
-      ok -> ok;
-      {error, {already_started, App}} -> ok;
-      {error, Error} ->
-	  throw({error, {cannot_start, App, {error, Error}}})
-    end.
+check_fun(S) ->
+    binary_to_list(iolist_to_binary(S)).
