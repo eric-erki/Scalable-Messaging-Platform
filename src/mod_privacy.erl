e7593c03436e2a31690aef2f7d3f6d5fc783b7ab
Zbigniew Żółkiewski <zzolkiewski@process-one.net>
2013-09-14 17:04:19 +0200

    Fix for export function

--- src/mod_privacy.erl
+++ src/mod_privacy.erl
@@ -1150,7 +1150,7 @@ update_table() ->
     end.
 
 export(Server) ->
-    case ejabberd_odbc:sql_query(jlib:nameprep(Server),
+    case catch ejabberd_odbc:sql_query(jlib:nameprep(Server),
 				 [<<"select id from privacy_list order by "
 				    "id desc limit 1;">>]) of
         {selected, [<<"id">>], [[I]]} ->
