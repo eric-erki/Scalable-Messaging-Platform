8cd9603eec1cbd03d35ebac54bdf5c2ef4e9ff62
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2013-11-11 12:13:29 +1000

    Disable SASL error logger if lager is enabled

--- src/ejabberd_logger.erl
+++ src/ejabberd_logger.erl
@@ -63,6 +63,8 @@ get_log_path() ->
 -ifdef(LAGER).
 
 start() ->
+    application:load(sasl),
+    application:set_env(sasl, sasl_error_logger, false),
     application:load(lager),
     ConsoleLog = get_log_path(),
     Dir = filename:dirname(ConsoleLog),
@@ -83,8 +85,7 @@ reopen_log() ->
               whereis(lager_event) ! {rotate, File};
          (_) ->
               ok
-      end, gen_event:which_handlers(lager_event)),
-    reopen_sasl_log().
+      end, gen_event:which_handlers(lager_event)).
 
 get() ->
     case lager:get_loglevel(lager_console_backend) of
@@ -146,8 +147,6 @@ get() ->
 set(LogLevel) ->
     p1_loglevel:set(LogLevel).
 
--endif.
-
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
@@ -171,3 +170,5 @@ get_sasl_error_logger_type () ->
 	{ok, Bad} -> exit ({bad_config, {sasl, {errlog_type, Bad}}});
 	_ -> all
     end.
+
+-endif.
