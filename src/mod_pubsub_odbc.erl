8ee3297786feb11ee4de574a6fbffcbdfd25c378
Christophe Romain <christophe.romain@process-one.net>
2014-02-11 19:17:43 +0100

    allow acl propagation on auto-create for pubsub_odbc

--- src/mod_pubsub_odbc.erl
+++ src/mod_pubsub_odbc.erl
@@ -1622,7 +1622,7 @@ iq_pubsub(Host, ServerHost, From, IQType, SubEl, Lang, Access, Plugins) ->
 		  [#xmlel{name = <<"item">>, attrs = ItemAttrs,
 			  children = Payload}] ->
 		      ItemId = xml:get_attr_s(<<"id">>, ItemAttrs),
-		      publish_item(Host, ServerHost, Node, From, ItemId, Payload);
+		      publish_item(Host, ServerHost, Node, From, ItemId, Payload, Access);
 		  [] ->
 		      {error,
 		       extended_error(?ERR_BAD_REQUEST, <<"item-required">>)};
@@ -2610,9 +2610,11 @@ unsubscribe_node(Host, Node, From, Subscriber, SubId) ->
     %%%
      | {error, xmlel()}
 ).
-publish_item(Host, ServerHost, Node, Publisher, <<>>, Payload) ->
-    publish_item(Host, ServerHost, Node, Publisher, uniqid(), Payload);
 publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload) ->
+	publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload, all).
+publish_item(Host, ServerHost, Node, Publisher, <<>>, Payload, Access) ->
+    publish_item(Host, ServerHost, Node, Publisher, uniqid(), Payload, Access);
+publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload, Access) ->
     Action = fun (#pubsub_node{options = Options, type = Type, id = NodeId}) ->
 		     Features = features(Type),
 		     PublishFeature = lists:member(<<"publish">>, Features),
@@ -2710,7 +2712,7 @@ publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload) ->
 	  Type = select_type(ServerHost, Host, Node),
 	  case lists:member(<<"auto-create">>, features(Type)) of
 	    true ->
-		case create_node(Host, ServerHost, Node, Publisher, Type) of
+		case create_node(Host, ServerHost, Node, Publisher, Type, Access, []) of
 		  {result,
 		   [#xmlel{name = <<"pubsub">>,
 			   attrs = [{<<"xmlns">>, ?NS_PUBSUB}],
