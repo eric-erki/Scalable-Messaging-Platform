84bff6e1100851089a184d8f0f47d3dea8dbbbff
Christophe Romain <christophe.romain@process-one.net>
2014-02-24 18:05:58 +0100

    added licence header

--- src/mod_c2s_debug.erl
+++ src/mod_c2s_debug.erl
@@ -1,3 +1,34 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_s2s_debug.erl
+%%% Author  : Mickael Remond <mremond@process-one.net>
+%%% Purpose : Log all s2s connections in a file
+%%% Created :  14 Mar 2008 by Mickael Remond <mremond@process-one.net>
+%%% Usage   : Add the following line in modules section of ejabberd.cfg:
+%%%              {mod_s2s_debug, [{filename, "/path/to/s2s.log"}]}
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2002-2013   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
+%%%----------------------------------------------------------------------
+-module(mod_c2s_debug).
+
+-author('mremond@process-one.net').
+
 %% Usage:
 %% In config file:
 %% {mod_c2s_debug, [{logdir, "/tmp/xmpplogs"}]},
@@ -10,12 +41,8 @@
 %% mod_c2s_debug:stop("localhost").
 %%
 %% Warning: Only one module for the debug handler can be defined.
--module(mod_c2s_debug).
-
--author('mremond@process-one.net').
 
 -behaviour(gen_mod).
-
 -behavior(gen_server).
 
 -export([start/2, start_link/2, stop/1, debug_start/3,
