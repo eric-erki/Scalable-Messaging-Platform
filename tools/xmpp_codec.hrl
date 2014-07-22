5d90a8fa671ee99f2ae980ca2736910037c67476
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2014-07-22 13:59:31 +0400

    Add tests for session management

--- tools/xmpp_codec.hrl
+++ tools/xmpp_codec.hrl
@@ -16,6 +16,9 @@
                      host :: binary(),
                      port = 1080 :: non_neg_integer()}).
 
+-record(sm_resume, {h :: non_neg_integer(),
+                    previd :: binary()}).
+
 -record(carbons_enable, {}).
 
 -record(carbons_private, {}).
@@ -41,10 +44,18 @@
                       from :: any(),
                       to :: any()}).
 
+-record(sm_a, {h :: non_neg_integer()}).
+
 -record(starttls_proceed, {}).
+
+-record(sm_resumed, {h :: non_neg_integer(),
+                     previd :: binary()}).
 
 -record(forwarded, {delay :: #delay{},
                     sub_els = [] :: [any()]}).
+
+-record(sm_enable, {max :: non_neg_integer(),
+                    resume = false :: any()}).
 
 -record(starttls_failure, {}).
 
@@ -55,6 +66,8 @@
 -record(private, {xml_els = [] :: [any()]}).
 
 -record(p1_ack, {}).
+
+-record(feature_sm, {}).
 
 -record(pubsub_item, {id :: binary(),
                       xml_els = [] :: [any()]}).
@@ -75,6 +88,8 @@
                             node :: binary(),
                             publisher :: binary()}).
 
+-record(sm_r, {}).
+
 -record(stat, {name :: binary(),
                units :: binary(),
                value :: binary(),
@@ -89,13 +104,18 @@
 -record(last, {seconds :: non_neg_integer(),
                text :: binary()}).
 
+-record(redirect, {uri :: binary()}).
+
+-record(sm_enabled, {id :: binary(),
+                     location :: binary(),
+                     max :: non_neg_integer(),
+                     resume = false :: any()}).
+
 -record(pubsub_event_items, {node :: binary(),
                              retract = [] :: [binary()],
                              items = [] :: [#pubsub_event_item{}]}).
 
 -record(pubsub_event, {items = [] :: [#pubsub_event_items{}]}).
-
--record(redirect, {uri :: binary()}).
 
 -record(sasl_response, {text :: any()}).
 
@@ -385,6 +405,8 @@
                      xdata = [] :: [#xdata{}]}).
 
 -record(sasl_mechanisms, {list = [] :: [binary()]}).
+
+-record(sm_failed, {reason :: atom() | #gone{} | #redirect{}}).
 
 -record(error, {type :: 'auth' | 'cancel' | 'continue' | 'modify' | 'wait',
                 by :: binary(),
