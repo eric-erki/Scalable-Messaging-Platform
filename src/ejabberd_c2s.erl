6de9dfb777e282dc818fb1f42994211e7dad28e0
Paweł Chmielowski <pawel@process-one.net>
2015-02-25 00:10:50 +0100

    Try not to use c2s:send_text, this don't work to good with websocket

--- src/ejabberd_c2s.erl
+++ src/ejabberd_c2s.erl
@@ -4016,9 +4016,8 @@ send_stanza_and_ack_req(StateData, Stanza) ->
     AckReq = #xmlel{name = <<"r">>,
 		    attrs = [{<<"xmlns">>, StateData#state.mgmt_xmlns}],
 		    children = []},
-    StanzaS = xml:element_to_binary(Stanza),
-    AckReqS = xml:element_to_binary(AckReq),
-    send_text(StateData, [StanzaS, AckReqS]).
+    send_element(StateData, Stanza),
+    send_element(StateData, AckReq).
 
 mgmt_queue_add(StateData, El) ->
     NewNum = case StateData#state.mgmt_stanzas_out of
@@ -4354,4 +4353,3 @@ format_features([El | Els], N) ->
             true -> <<"\n">>
         end,
     [El, {xmlcdata, S} | format_features(Els, N + 1)].
-
