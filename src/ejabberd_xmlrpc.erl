cd0c4e8bbdcb43604e1138f8e462d712dfa6aa5b
Badlop <badlop@process-one.net>
2015-05-29 15:30:03 +0200

    Improve mod_http_admin for full ejabberd_commands support (TECH-1752)

--- src/ejabberd_xmlrpc.erl
+++ src/ejabberd_xmlrpc.erl
@@ -460,10 +460,10 @@ format_arg({array, Elements}, {list, ElementsDef})
 format_arg(Arg, integer) when is_integer(Arg) -> Arg;
 format_arg(Arg, binary) when is_list(Arg) -> process_unicode_codepoints(Arg);
 format_arg(Arg, binary) when is_binary(Arg) -> Arg;
-format_arg(Arg, string) when is_list(Arg) -> process_unicode_codepoints(Arg);
-format_arg(Arg, string) when is_binary(Arg) -> Arg;
+format_arg(Arg, string) when is_list(Arg) -> Arg;
+format_arg(Arg, string) when is_binary(Arg) -> binary_to_list(Arg);
 format_arg(undefined, binary) -> <<>>;
-format_arg(undefined, string) -> <<>>;
+format_arg(undefined, string) -> "";
 format_arg(Arg, Format) ->
     ?ERROR_MSG("don't know how to format Arg ~p for format ~p", [Arg, Format]),
     error.
