e40eab97ee8beb9e3a73a164250a816504b52b63
Paweł Chmielowski <pchmielowski@process-one.net>
2015-10-26 12:25:09 +0100

    Faster jlib:string_to_usr implementation
    This version is about 30% faster than previous one.

--- c_src/jlib.c
+++ c_src/jlib.c
@@ -1,37 +1,49 @@
 #include <erl_nif.h>
 #include <string.h>
 
-#define STATE_NODE 1
-#define STATE_SERVER 2
-#define STATE_RESOURCE 3
-
 static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
 {
   return 0;
 }
 
-static ERL_NIF_TERM make_error(ErlNifEnv* env,
-			       ErlNifBinary* input,
-			       ErlNifBinary* node,
-			       ErlNifBinary* server,
-			       ErlNifBinary* resource)
+static ERL_NIF_TERM *create_term(ErlNifEnv* env, ERL_NIF_TERM *term, unsigned char* start,
+                                 unsigned char* end, ERL_NIF_TERM* empty, int* initialized)
 {
+  if (start == end) {
+    if (!*initialized) {
+        enif_make_new_binary(env, 0, empty);
+        *initialized = 1;
+    }
+    return empty;
+  }
+  memcpy(enif_make_new_binary(env, end-start, term), start, end-start);
+
+  return term;
+}
+
+static ERL_NIF_TERM split_str(ErlNifEnv* env, ErlNifBinary* input,
+                              unsigned char* split1, unsigned char* split2, unsigned char* end)
+{
+  ERL_NIF_TERM node, domain, resource, empty;
+  ERL_NIF_TERM *nodep, *domainp, *resourcep;
+  int empty_initialized = 0;
+  unsigned char *start = input->data;
+
+  nodep = create_term(env, &node, start, split1, &empty, &empty_initialized);
+  domainp = create_term(env, &domain, start==split1||split1==split2 ? split1 : split1+1, split2, &empty, &empty_initialized);
+  resourcep = create_term(env, &resource, split1==split2||split2==end ? split2 : split2+1, end, &empty, &empty_initialized);
   enif_release_binary(input);
-  enif_release_binary(node);
-  enif_release_binary(server);
-  enif_release_binary(resource);
-  return enif_make_atom(env, "error");
+
+  return enif_make_tuple3(env,
+                          *nodep,
+                          *domainp,
+                          *resourcep);
 }
 
 static ERL_NIF_TERM string_to_usr(ErlNifEnv* env, int argc,
 				  const ERL_NIF_TERM argv[])
 {
-  int pos = 0;
-  int amp = 0;
-  int slash = 0;
-  int state = STATE_NODE;
-  char chr;
-  ErlNifBinary input, node, server, resource;
+  ErlNifBinary input;
 
   if (argc != 1)
     return enif_make_badarg(env);
@@ -42,65 +54,58 @@ static ERL_NIF_TERM string_to_usr(ErlNifEnv* env, int argc,
   if (input.size == 0)
     return enif_make_atom(env, "error");
 
-  enif_alloc_binary(0, &node);
-  enif_alloc_binary(0, &server);
-  enif_alloc_binary(0, &resource);
+  unsigned char *end = input.data+input.size;
+  unsigned char *c = input.data;
 
-  while (pos < input.size) {
-
-    if (state == STATE_NODE) {
-      chr = input.data[pos];
-      if (chr == '/')
-	state = STATE_SERVER;
-      if (chr == '@') {
-	if (pos == 0 || pos+1 == input.size)
-	  return make_error(env, &input, &node, &server, &resource);
-	enif_realloc_binary(&node, pos);
-	memcpy(node.data, input.data, pos);
-	amp = ++pos;
-	state = STATE_SERVER;
-      }
-    }
-
-    if (state == STATE_SERVER) {
-      chr = input.data[pos];
-      if (chr == '@')
-	return make_error(env, &input, &node, &server, &resource);
-      if (chr == '/') {
-	if (pos == 0 || pos+1 == input.size || pos == amp)
-	  return make_error(env, &input, &node, &server, &resource);
-	enif_realloc_binary(&server, pos - amp);
-	memcpy(server.data, input.data + amp, pos - amp);
-	slash = ++pos;
-	state = STATE_RESOURCE;
-      }
-    }
-
-    if (state == STATE_RESOURCE)
+  while (c < end) {
+    if (*c == '/' || *c == '@')
       break;
-
-    pos++;
+    c++;
   }
 
-  switch (state) {
-  case STATE_NODE:
-    enif_realloc_binary(&server, input.size);
-    memcpy(server.data, input.data, input.size);
-    break;
-  case STATE_SERVER:
-    enif_realloc_binary(&server, input.size - amp);
-    memcpy(server.data, input.data + amp, input.size - amp);
-    break;
-  case STATE_RESOURCE:
-    enif_realloc_binary(&resource, input.size - slash);
-    memcpy(resource.data, input.data + slash, input.size - slash);
-    break;
+  if (c == end) {
+    ERL_NIF_TERM empty;
+    enif_make_new_binary(env, 0, &empty);
+
+    return enif_make_tuple3(env,
+                            empty,
+                            enif_make_binary(env, &input),
+                            empty);
   }
 
-  return enif_make_tuple3(env,
-			  enif_make_binary(env, &node),
-			  enif_make_binary(env, &server),
-			  enif_make_binary(env, &resource));
+  if (c == input.data || c+1 == end) {
+    enif_release_binary(&input);
+    return enif_make_atom(env, "error");
+  }
+
+  if (*c == '/') {
+    return split_str(env, &input, input.data, c, end);
+  }
+
+  unsigned char *c1 = c;
+  c++;
+
+  while (c < end) {
+    if (*c == '/' || *c == '@')
+      break;
+    c++;
+  }
+
+  if (c == end) {
+    return split_str(env, &input, c1, end, end);
+  }
+
+  if (c == c1+1 || c+1 == end) {
+    enif_release_binary(&input);
+    return enif_make_atom(env, "error");
+  }
+
+  if (*c == '@') {
+    enif_release_binary(&input);
+    return enif_make_atom(env, "error");
+  }
+
+  return split_str(env, &input, c1, c, end);
 }
 
 static ErlNifFunc nif_funcs[] =
