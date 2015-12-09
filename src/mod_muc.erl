f421a9392e03ef13040d5bf2a534a5a4ca2b1ae8
Jerome Sautret <jerome.sautret@process-one.net>
2015-12-09 15:31:25 +0100

    resolve merge of 797efaa (EJABS-2568)

--- src/mod_muc.erl
+++ src/mod_muc.erl
@@ -337,13 +337,8 @@ can_use_nick(_LServer, Host, JID, Nick, p1db) ->
         {error, _} ->
             true
     end;
-<<<<<<< HEAD
-can_use_nick(LServer, Host, JID, Nick, riak) ->
-    {LUser, LServer, _} = jid:tolower(JID),
-=======
 can_use_nick(_LServer, Host, JID, Nick, riak) ->
-    {LUser, LServer, _} = jlib:jid_tolower(JID),
->>>>>>> 8ed1801... Fix MUC join from S2S connection with riak backend (EJABS-2568)
+    {LUser, LServer, _} = jid:tolower(JID),
     LUS = {LUser, LServer},
     case ejabberd_riak:get_by_index(muc_registered,
 				    muc_registered_schema(),
