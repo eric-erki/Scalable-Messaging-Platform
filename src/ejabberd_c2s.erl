fe73553e3f1171c934b606965272b07bfee837ed
Holger Weiss <holger@zedat.fu-berlin.de>
2016-06-19 23:32:15 +0200

    Fix handling of queued stanzas on session timeout
    Don't fail to resend or bounce unacknowledged stanzas if the stream
    management session timed out.
    
    Closes #1160.

--- src/ejabberd_c2s.erl
+++ src/ejabberd_c2s.erl
@@ -3716,7 +3716,8 @@ check_queue_length(#state{mgmt_queue = Queue,
 
 handle_unacked_stanzas(StateData, F)
   when StateData#state.mgmt_state == active;
-       StateData#state.mgmt_state == pending ->
+	 StateData#state.mgmt_state == pending;
+	 StateData#state.mgmt_state == timeout ->
     Queue = StateData#state.mgmt_queue,
     case queue:len(Queue) of
 	0 ->
@@ -3738,7 +3739,8 @@ handle_unacked_stanzas(_StateData, _F) ->
 
 handle_unacked_stanzas(StateData)
   when StateData#state.mgmt_state == active;
-       StateData#state.mgmt_state == pending ->
+	 StateData#state.mgmt_state == pending;
+	 StateData#state.mgmt_state == timeout ->
     ResendOnTimeout =
 	case StateData#state.mgmt_resend of
 	    Resend when is_boolean(Resend) ->
