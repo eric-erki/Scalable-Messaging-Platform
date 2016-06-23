4c1c1137be5598d2371e2846b2030e0a29c978c2
Holger Weiss <holger@zedat.fu-berlin.de>
2016-06-23 14:23:24 +0200

    mod_pubsub: Fix matching of set_node/1 result
    nodetree_tree_sql:set_node/1 returns {result, NodeIdx} on success, not
    {ok, NodeIdx}.  Thanks to Christophe Romain for spotting this.

--- src/mod_pubsub.erl
+++ src/mod_pubsub.erl
@@ -3843,7 +3843,7 @@ set_configure(Host, Node, From, Els, Lang) ->
 							    set_node,
 							    [N#pubsub_node{options = NewOpts}])
 						    of
-							{ok, Nidx} -> {result, ok};
+							{result, Nidx} -> {result, ok};
 							ok -> {result, ok};
 							Err -> Err
 						    end;
