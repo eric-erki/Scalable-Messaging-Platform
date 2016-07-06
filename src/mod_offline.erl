b1b28efc056e50a205521d33d8faed10c5650f01
Badlop <badlop@process-one.net>
2016-07-06 17:54:37 +0200

    Revert "Recover fix of 907e239 lost in 9deb294 (thanks to Alexey Shchepin)" (#1183)
    This reverts commit 53f3a4580312ee503bfec1e2e14444ecad931fdd.

--- src/mod_offline.erl
+++ src/mod_offline.erl
@@ -761,7 +761,7 @@ get_messages_subset(User, Host, MsgsAll) ->
                                     fun(A) -> A end,
 				    max_user_offline_messages),
     MaxOfflineMsgs = case get_max_user_messages(Access,
-						{User, Host}, Host)
+						User, Host)
 			 of
 		       Number when is_integer(Number) -> Number;
 		       _ -> 100
