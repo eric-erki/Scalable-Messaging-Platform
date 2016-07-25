c92bd41d8ce244a0a59f4d6139805b932c943cb0
Mickael Remond <mremond@process-one.net>
2016-07-25 18:28:05 +0200

    Simplify code for command policy group expansion

--- src/ejabberd_commands.erl
+++ src/ejabberd_commands.erl
@@ -519,6 +519,7 @@ execute_command(AccessCommands1, Auth1, Name, Arguments, Version, CallerInfo) ->
     TokenJID = oauth_token_user(Auth1),
     Command = get_command_definition(Name, Version),
     AccessCommands = get_all_access_commands(AccessCommands1),
+
     case check_access_commands(AccessCommands, Auth, Name, Command, Arguments, CallerInfo) of
         ok -> execute_check_policy(Auth, TokenJID, Command, Arguments)
     end.
@@ -794,19 +795,15 @@ get_commands(Version) ->
           end, [], Opts),
     Cmds.
 
+%% This is used to allow mixing command policy (like open, user, admin, restricted), with command entry
 expand_commands(L, OpenCmds, UserCmds, AdminCmds, RestrictedCmds) when is_list(L) ->
-    lists:foldl(fun(El, Acc) ->
-                        expand_commands(El, OpenCmds, UserCmds, AdminCmds, RestrictedCmds) ++ Acc
-                end, [], L);
-expand_commands(El, OpenCmds, UserCmds, AdminCmds, RestrictedCmds) ->
-    case El of
-                             open -> OpenCmds;
-                             restricted -> RestrictedCmds;
-                             admin -> AdminCmds;
-                             user -> UserCmds;
-        _ -> [El]
-    end.
-
+    lists:foldl(fun(open, Acc) -> OpenCmds ++ Acc;
+                   (user, Acc) -> UserCmds ++ Acc;
+                   (admin, Acc) -> AdminCmds ++ Acc;
+                   (restricted, Acc) -> RestrictedCmds ++ Acc;
+                   (Command, Acc) when is_atom(Command) ->
+                        [Command, Acc]
+                end, [], L).
 
 oauth_token_user(noauth) ->
     undefined;
