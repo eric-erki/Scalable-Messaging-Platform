processone/ejabberd

fix clean redis table

src/ejabberd_sm_redis.erl
@@ -145,7 +145,10 @@ clean_table() ->
 					{_, SID} = binary_to_term(USSIDKey),
 					node(element(2, SID)) == node()
 				end, Vals),
-		      Q1 = ["HDEL", ServKey | Vals1],
+				Q1 = case Vals1 of
+					[] -> [];
+					_ -> ["HDEL", ServKey | Vals1]
+				end,
 		      Q2 = lists:map(
 			     fun(USSIDKey) ->
 				     {US, SID} = binary_to_term(USSIDKey),
@@ -153,7 +156,7 @@ clean_table() ->
 				     SIDKey = sid_to_key(SID),
 				     ["HDEL", USKey, SIDKey]
 			     end, Vals1),
-		      Res = ejabberd_redis:qp([Q1|Q2]),
+		      Res = ejabberd_redis:qp(lists:delete([], [Q1|Q2])),
 		      case lists:filter(
 			     fun({ok, _}) -> false;
 				(_) -> true

eric-erki/Scalable-Messaging-Platform

Conflicts:
	src/ejabberd_sm_redis.erl

src/ejabberd_sm_redis.erl
@@ -139,14 +139,17 @@ delete_node(Node) ->
                                         end
 				end, Vals),
                       Vals2 = [usr_to_key(S#session.usr) || S <- Vals1],
-		      Q1 = ["HDEL", ServKey | Vals2],
+		      Q1 = case Vals2 of
+			       [] -> [];
+			       _ -> ["HDEL", ServKey | Vals2]
+			   end,
 		      Q2 = lists:map(
 			     fun(S) ->
                                      {_, _, R} = S#session.usr,
 				     USKey = us_to_key(S#session.us),
 				     ["HDEL", USKey, R]
 			     end, Vals1),
-		      Res = ejabberd_redis:qp([Q1|Q2]),
+		      Res = ejabberd_redis:qp(lists:delete([], [Q1|Q2])),
 		      case lists:filter(
 			     fun({ok, _}) -> false;
 				(_) -> true