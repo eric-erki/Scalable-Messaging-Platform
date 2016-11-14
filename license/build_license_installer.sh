1523d0c970c28c7e22439129b8fa6354a8147162
Christophe Romain <christophe.romain@process-one.net>
2016-11-14 15:23:01 +0100

    Fix build_license_installer used path

--- license/build_license_installer.sh
+++ license/build_license_installer.sh
@@ -2,15 +2,16 @@
 
 [ $# -eq 3 ] || exit 4
 
+dir=$(dirname $0)
 installer=$1
 version=$2
 license=$3
 
-size=$(wc -c tools/ejabberd_patch.sh | cut -d' ' -f1)
+size=$(wc -c $dir/ejabberd_patch.sh | cut -d' ' -f1)
 offset=$((size%512))
-cp tools/ejabberd_patch.sh $installer.run
+cp $dir/ejabberd_patch.sh $installer.run
 dd if=/dev/zero bs=1 count=$((512-offset)) >> $installer.run 2>/dev/null
-tools/build_license.es $version $license
-erlc tools/ejabberd_patch.erl
+$dir/build_license.es $version $license
+erlc $dir/ejabberd_patch.erl
 dd if=ejabberd_patch.beam bs=512 >> $installer.run 2>/dev/null
 rm ejabberd_patch.beam ejabberd_patch.hrl
